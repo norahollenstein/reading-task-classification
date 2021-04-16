97ee19e0496c7ea123498be457f4aeeb18d59a22
Nora Hollenstein <nora.hollenstein@gmail.com>
2021-04-16 11:15:27 +0200

    flesch baseline

--- sentence-level/config.py
+++ sentence-level/config.py
@@ -1,6 +1,6 @@
 
 # dataset {zuco1, zuco2, zucoAll, zuco1sr}
-dataset = 'zuco2'
+dataset = 'zucoAll'
 
 if dataset is 'zuco2':
     # todo: what about missing subjects?
@@ -12,6 +12,7 @@ elif dataset is 'zuco1' or dataset is 'zuco1sr':
     rootdir = "/mnt/ds3lab-scratch/noraho/datasets/zuco/zuco1_preprocessed_sep2020/"
 elif dataset is "zucoAll":
     subjects = ['YAC', 'YAG', 'YAK', 'YDG', 'YDR', 'YFR', 'YFS', 'YHS', 'YIS', 'YLS', 'YMD', 'YRK', 'YRP', 'YSD', 'YSL', 'YTL', "ZDN", "ZPH", "ZJN", "ZAB", "ZJM", "ZKB", "ZKH", "ZMG", "ZGW", "ZKW", "ZDM"] #'YMS', 'YRH', #'ZJS
+    subjects = ["ZDN", "ZPH", "ZJN", "ZAB", "ZJM", "ZKB", "ZKH", "ZMG", "ZGW", "ZKW", "ZDM"] #"ZJS"
     rootdir2 = "/mnt/ds3lab-scratch/noraho/datasets/zuco/zuco2_preprocessed_sep2020/"
     rootdir1 = "/mnt/ds3lab-scratch/noraho/datasets/zuco/zuco1_preprocessed_sep2020/"
 
@@ -26,14 +27,16 @@ seed = 1
 runs = 50
 
 # randomize labels as a sanity check; default = False
-randomized_labels = True
+randomized_labels = False
 
 if level is 'sentence':
     # sentence-level eye-tracking feature sets
     #feature_sets = ["fixation_number", "omission_rate", "reading_speed", 'sent_gaze', "mean_sacc_dur", "max_sacc_velocity", "mean_sacc_velocity", "max_sacc_dur", 'sent_saccade', 'sent_gaze_sacc']
     # sentence-level EEG feature sets
     #feature_sets = ["theta_mean", "alpha_mean", "beta_mean", "gamma_mean", "eeg_means", "sent_gaze_eeg_means", "electrode_features_theta", "electrode_features_alpha", "electrode_features_beta", "electrode_features_gamma", "electrode_features_all"]
-    feature_sets = ["flesch_baseline"]
+    feature_sets = ["electrode_features_all"]
+    # sentence-level baseline feature
+    #feature_sets = ["flesch_baseline"]
 
 
 elif level is 'word':
