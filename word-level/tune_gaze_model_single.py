d129f8ed0e6edea7e4d3a649dba195d4e829cc3a
Nora Hollenstein <nora.hollenstein@gmail.com>
2021-05-26 17:35:19 +0200

    ..

--- word-level/tune_gaze_model_single.py
+++ word-level/tune_gaze_model_single.py
@@ -35,7 +35,6 @@ def main():
         with open("features/" + subject + "_" + config.feature_set[0] + '_feats_file_'+config.class_task+'_Sacc'+str(config.saccades)+'.json', 'w') as fp:
            json.dump(gaze_dict, fp)
         print("saved.")
-        sys.exit()
 
         feature_dict = collections.OrderedDict(sorted(feature_dict.items()))
         label_dict = collections.OrderedDict(sorted(label_dict.items()))
