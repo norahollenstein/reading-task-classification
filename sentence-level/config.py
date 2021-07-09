cceb918019994d5eb0197e9f1176b8df35ecb682
Nora Hollenstein <noraho@spaceml3.ethz.ch>
2021-07-09 14:39:40 +0200

    ..

--- sentence-level/config.py
+++ sentence-level/config.py
@@ -1,14 +1,14 @@
 
 # dataset {zuco1, zuco2, zucoAll, zuco1sr}
-dataset = 'zucoAll'
+dataset = 'zuco1'
 
 if dataset is 'zuco2':
     # todo: what about missing subjects?
-    subjects = ['YAC']#, 'YAG', 'YAK', 'YDG', 'YDR', 'YFR', 'YFS', 'YHS', 'YIS', 'YLS', 'YMD', 'YRK', 'YRP', 'YSD', 'YSL', 'YTL']  # exclude YMH,  - YRH, YMS
+    subjects = ['YAC', 'YAG', 'YAK', 'YDG', 'YDR', 'YFR', 'YFS', 'YHS', 'YIS', 'YLS', 'YMD', 'YRK', 'YRP', 'YSD', 'YSL', 'YTL']  # exclude YMH,  - YRH, YMS
     # new preprocessed data Sept. 2020
     rootdir = "/mnt/ds3lab-scratch/noraho/datasets/zuco/zuco2_preprocessed_sep2020/"
 elif dataset is 'zuco1' or dataset is 'zuco1sr':
-    subjects = ["ZDN"]#, "ZPH", "ZJN", "ZAB", "ZJM", "ZKB", "ZKH", "ZMG", "ZGW", "ZKW", "ZDM"] #"ZJS"
+    subjects = ["ZDN", "ZPH", "ZJN", "ZAB", "ZJM", "ZKB", "ZKH", "ZMG", "ZGW", "ZKW", "ZDM"] #"ZJS"
     rootdir = "/mnt/ds3lab-scratch/noraho/datasets/zuco/zuco1_preprocessed_sep2020/"
 elif dataset is "zucoAll":
     subjects = ['YAC', 'YAG', 'YAK', 'YDG', 'YDR', 'YFR', 'YFS', 'YHS', 'YIS', 'YLS', 'YMD', 'YRK', 'YRP', 'YSD', 'YSL', 'YTL', "ZDN", "ZPH", "ZJN", "ZAB", "ZJM", "ZKB", "ZKH", "ZMG", "ZGW", "ZKW", "ZDM"] #'YMS', 'YRH', #'ZJS
@@ -20,7 +20,7 @@ elif dataset is "zucoAll":
 # level {word, sentence}
 # sentence-level: SVM used for classification
 # word-level: Only data from fixations inside wordbounds
-level = 'word'
+level = 'sentence'
 
 # experiment setup
 seed = 1
@@ -31,10 +31,10 @@ randomized_labels = False
 
 if level is 'sentence':
     # sentence-level eye-tracking feature sets
-    #feature_sets = ["fixation_number", "omission_rate", "reading_speed", 'sent_gaze', "mean_sacc_dur", "max_sacc_velocity", "mean_sacc_velocity", "max_sacc_dur", 'sent_saccade', 'sent_gaze_sacc']
-    feature_sets = ["max_sacc_amp", "mean_sacc_amp", 'sent_saccade', 'sent_gaze_sacc']
+    #feature_sets = ["fixation_number", "omission_rate", "reading_speed", 'sent_gaze', "mean_sacc_dur", "max_sacc_velocity", "mean_sacc_velocity", "max_sacc_dur", "max_sacc_amp", "mean_sacc_amp", 'sent_saccade', 'sent_gaze_sacc']
+    #feature_sets = ["max_sacc_amp", "mean_sacc_amp", 'sent_saccade', 'sent_gaze_sacc']
     # sentence-level EEG feature sets
-    #feature_sets = ["theta_mean", "alpha_mean", "beta_mean", "gamma_mean", "eeg_means", "sent_gaze_eeg_means", "electrode_features_theta", "electrode_features_alpha", "electrode_features_beta", "electrode_features_gamma", "electrode_features_all"]
+    feature_sets = ["theta_mean", "alpha_mean", "beta_mean", "gamma_mean", "eeg_means", "sent_gaze_eeg_means", "electrode_features_theta", "electrode_features_alpha", "electrode_features_beta", "electrode_features_gamma", "electrode_features_all"]
     #feature_sets = ["electrode_features_all"]
     # sentence-level baseline feature
     #feature_sets = ["flesch_baseline"]
@@ -46,7 +46,7 @@ elif level is 'word':
     feature_sets = ['fix_order_raw_eeg_electrodes_10%']#, 'fix_order_raw_eeg_electrodes_20%', 'fix_order_raw_eeg_electrodes_50%', 'fix_order_raw_eeg_electrodes_75%', 'fix_order_raw_eeg_electrodes']
 
 # classification task {tasks, sessions, subjects}
-class_task = 'tasks'
+class_task = 'subjects'
 if class_task == 'tasks' or class_task == 'tasks-cross-subj':
     target_names = ["TSR", "NR"]
     labels = [0, 1]
