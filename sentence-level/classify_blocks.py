6ea88b70b5043e5b3b162ae1dbbb9e3220216a18
Nora Hollenstein <nora.hollenstein@gmail.com>
2021-07-21 13:13:48 +0200

    ..

--- sentence-level/classify_blocks.py
+++ sentence-level/classify_blocks.py
@@ -52,7 +52,7 @@ def main():
             predictions = []; true_labels = []; accuracies = []; svm_coeffs = []
             for i in range(config.runs):
                 #print(i)
-                preds, test_y, acc, coefs = classifier.svm(features[feature_set], config.seed+i, config.randomized_labels)
+                preds, test_y, acc, coefs = classifier.svm_cross_subj(features[feature_set], config.seed+i, config.randomized_labels)
 
                 accuracies.append(acc)
                 predictions.extend(preds)
