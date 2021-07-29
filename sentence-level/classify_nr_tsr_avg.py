75ed4510c178dce0f004a1bc817424c351d8153a
Nora Hollenstein <nora.hollenstein@gmail.com>
2021-07-29 12:48:57 +0200

    topoplots

--- sentence-level/classify_nr_tsr_avg.py
+++ sentence-level/classify_nr_tsr_avg.py
@@ -81,8 +81,9 @@ def main():
         evoked_nr.set_montage("GSN-HydroCel-128")
 
         fig, ax = plt.subplots(figsize=(7.5, 4.5), nrows=1, ncols=1)
-        ax = evoked_nr.plot_topomap(title='EEG patterns', time_unit='s', units='a.u.', scalings=1, vmin=min(diff))
+        ax = evoked_nr.plot_topomap(title='EEG patterns', time_unit='s', units='a.u.', scalings=1, vmin=min(diff), cmap='RdBu')
         plt.savefig("NR-topo-AVG-ALL"+feature_set+".pdf")
+        plt.close()
 
 
 
@@ -94,7 +95,7 @@ def main():
         fig, ax = plt.subplots(figsize=(7.5, 4.5), nrows=1, ncols=1)
         ax = evoked_tsr.plot_topomap(title='EEG patterns', time_unit='s', units='a.u.', scalings=1, vmin=min(diff))
         plt.savefig("TSR-topo-AVG-ALL"+feature_set+".pdf")
-
+        plt.close()
 
 
         print(diff)
@@ -105,6 +106,7 @@ def main():
         fig, ax = plt.subplots(figsize=(7.5, 4.5), nrows=1, ncols=1)
         ax = evoked_diff.plot_topomap(title='EEG patterns', time_unit='s', units='a.u.', scalings=1, vmin=min(diff))
         plt.savefig("Diff-topo-AVG-ALL"+feature_set+".pdf")
+        plt.close()
 
 
         #print(features_avg)
