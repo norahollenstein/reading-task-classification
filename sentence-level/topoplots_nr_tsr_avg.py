e8a93a1bd968a8b4668dbbc50fdc11182bffc6c0
Nora Hollenstein <nora.hollenstein@gmail.com>
2021-07-29 16:24:24 +0200

    .

--- sentence-level/topoplots_nr_tsr_avg.py
+++ sentence-level/topoplots_nr_tsr_avg.py
@@ -60,7 +60,8 @@ def main():
 
         # NR
         evoked_nr = EvokedArray(mean_nr.reshape(-1,1), info=info)
-        #evoked_nr.set_montage("GSN-HydroCel-128")
+        evoked_nr.set_montage("GSN-HydroCel-128")
+        print(evoked_nr.info)
 
         fig, ax = plt.subplots(figsize=(7.5, 4.5), nrows=1, ncols=1)
         ax = evoked_nr.plot_topomap(title='EEG patterns', time_unit='s', units='a.u.', scalings=1, vmin=min(diff), cmap='RdBu')
